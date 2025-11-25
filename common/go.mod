e7a58c2e4e2585a6c261b2a8637507e7e0ceb629
JagTheFriend <JagTheFriend12@gmail.com>
2025-11-25 22:11:57 +0530

    Add RabbitMQ connection functionality and update module dependencies
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- common/go.mod
+++ common/go.mod
@@ -1,3 +1,5 @@
 module common
 
 go 1.25.3
+
+require github.com/rabbitmq/amqp091-go v1.10.0 // indirect
---
+++ common/rabbitmq.go
@@ -0,0 +1,36 @@
+package common
+
+import (
+	"fmt"
+	"log"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+const OrderCreatedEvent = "order.created"
+
+func ConnectRabbitAMQP(user, pass, host, port string) (*amqp.Connection, func() error) {
+	address := fmt.Sprintf("amqp://%s:%s@%s:%s/", user, pass, host, port)
+
+	conn, err := amqp.Dial(address)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	channel, err := conn.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = channel.ExchangeDeclare("exchange", "direct", true, false, false, false, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = channel.ExchangeDeclarePassive(OrderCreatedEvent, "fanout", true, false, false, false, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return conn, conn.Close
+}
